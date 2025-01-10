eaa46fd30108b8ebf8dcee132f15a08a59f4f8ec
aexyzk <aexyzk@proton.me>
2025-01-09 23:19:27 -0600

    ball

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+clear
+gcc main.c -o termpong -lncurses
+./termpong
--- main.c
+++ main.c
@@ -1,10 +1,42 @@
-#include <ncurses.h>
- 
-int main(void){	
-	initscr();			/* Start curses mode 		  */
-	printw("Hello World! Press any key to exit ...");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
-	return 0;
+#include <curses.h>
+#include <unistd.h>
+
+#define DELAY 30000
+
+int main()
+{
+	initscr();
+	noecho();
+	curs_set(FALSE);
+
+	int ballX = 0;
+	int ballY = 0;
+	int axisX = 1;
+	int axisY = 1;
+	int winX, winY;
+
+	getmaxyx(stdscr, winY, winX);
+
+	bool running = true;
+	while(running){
+		if (ballX >= winX || ballX < 0) {
+			axisX*= -1;
+		}
+		ballX += 1 * axisX;
+
+		if (ballY >= winY || ballY < 0) {
+			axisY*= -1;
+		}
+		ballY += 1 * axisY;
+
+		clear();
+		mvprintw(ballY,ballX,"O");
+		refresh();
+
+		usleep(DELAY);
+	}
+
+	endwin();
+
+    return 0;
 }
